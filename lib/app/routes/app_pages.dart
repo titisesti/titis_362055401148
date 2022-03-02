41c29530ecc9c943e39c6ca9b24f7e1e1f07e291
FlutLab.io <>
2022-03-02 16:17:21 +0000

    Initial commit

---
+++ lib/app/routes/app_pages.dart
@@ -0,0 +1,26 @@
+import 'package:get/get.dart';
+import 'package:titisesti/app/modules/home/bindings/home_binding.dart';
+import 'package:titisesti/app/modules/home/views/home_view.dart';
+import 'package:titisesti/app/modules/login/bindings/login_binding.dart';
+import 'package:titisesti/app/modules/login/views/login_view.dart';
+
+part 'app_routes.dart';
+
+class AppPages {
+  AppPages._();
+
+  static const INITIAL = Routes.HOME;
+
+  static final routes = [
+    GetPage(
+      name: _Paths.HOME,
+      page: () => HomeView(),
+      binding: HomeBinding(),
+    ),
+    GetPage(
+      name: _Paths.LOGIN,
+      page: () => LoginView(),
+      binding: LoginBinding(),
+    ),
+  ];
+}
---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'app/routes/app_pages.dart';
+import 'app/widgets/splash.dart';
+import 'package:titisesti/app/modules/login/views/login_view.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // return GetMaterialApp(
+    //   debugShowCheckedModeBanner: false,
+    //   title: "Application",
+    //   initialRoute: Routes.HOME,
+    //   getPages: AppPages.routes,
+    // );
+    return FutureBuilder(
+      future: Future.delayed(Duration(seconds: 3)),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return SplashScreen();
+        } else {
+          return GetMaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: "Application",
+            initialRoute: Routes.LOGIN,
+            getPages: AppPages.routes,
+          );
+        }
+      },
+    );
+  }
+}
